ebeb0e6ce99a11f001aeeff745840ec0734ac436
Michał Kobiera <55958194+MicelloK@users.noreply.github.com>
2024-05-20 11:00:54 +0200

    Time: 38 ms (83.52%) | Memory: 16.4 MB (84.04%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
@@ -1,10 +1,15 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        total_xor = 0
-        for i in range(2**len(nums)):
-            partial_xor = 0
-            for j in range(len(nums)):
-                if i & (1 << j):
-                    partial_xor ^= nums[j]
-            total_xor += partial_xor
-        return total_xor
+        a = 0
+        for i in nums:
+            a|=i
+        return a* (1<<(len(nums)-1))
+
+        # total_xor = 0
+        # for i in range(2**len(nums)):
+        #     partial_xor = 0
+        #     for j in range(len(nums)):
+        #         if i & (1 << j):
+        #             partial_xor ^= nums[j]
+        #     total_xor += partial_xor
+        # return total_xor
