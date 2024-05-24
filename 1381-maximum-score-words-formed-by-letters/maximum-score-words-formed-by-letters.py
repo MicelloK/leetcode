e18afb9ba9de2c9f9b243da4e457b658739eb992
Michał Kobiera <55958194+MicelloK@users.noreply.github.com>
2024-05-24 11:19:17 +0200

    Time: 63 ms (23.13%) | Memory: 17.2 MB (6.35%) - LeetSync

--- 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.py
+++ 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.py
@@ -1,31 +1,31 @@
 class Solution:
     def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
-        words_scores = {}
-        for word in words:
-            word_score = 0
-            for char in word:
-                word_score += score[ord(char)-97]
-            words_scores[word] = word_score
+        words_scores = {word: sum(score[ord(char)-97] for char in word) for word in words}
 
-        words_counter = {word: Counter(word) for word in words}
-
-        def visit(i: int, available) -> int:
+        def visit(i: int, available, memo) -> int:
             if i >= len(words):
                 return 0
 
-            max_val = visit(i+1, available)
+            state = (i, tuple(available.items()))
+            if state in memo:
+                return memo[state]
+
+            max_val = visit(i+1, available, memo)
 
             word = words[i]
+            word_counter = Counter(word)
             valid_word = True
-            for char, count in words_counter[word].items():
+            for char, count in word_counter.items():
                 if available[char] < count:
                     valid_word = False
 
             if valid_word:
-                new_available = available - words_counter[word]
-                max_val = max(max_val, words_scores[word] + visit(i+1, new_available))
+                new_available = available - word_counter
+                max_val = max(max_val, words_scores[word] + visit(i+1, new_available, memo))
 
+            memo[state] = max_val
             return max_val
 
         available = Counter(letters)
-        return visit(0, available)
+        memo = {}
+        return visit(0, available, memo)
