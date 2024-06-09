eb1ff31185d85e428a29edff7b79b729a2738607
Michał Kobiera <55958194+MicelloK@users.noreply.github.com>
2024-06-09 10:29:25 +0200

    Time: 257 ms (5.25%) | Memory: 21.4 MB (87.31%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -1,7 +1,7 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
         result = 0
-        occurrenced = {}
+        occurrenced = {0: 1}
         prefix_sum = 0
         for num in nums:
             prefix_sum += num
@@ -11,7 +11,5 @@ class Solution:
                 occurrenced[prefix_sum] += 1
             else:
                 occurrenced[prefix_sum] = 1
-            if prefix_sum == 0:
-                result += 1
         return result
 
