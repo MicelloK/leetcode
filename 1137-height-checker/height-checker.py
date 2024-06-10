7a7d49dc7ed92da4ee1b6a5e72e4b4c15b62dfad
Michał Kobiera <55958194+MicelloK@users.noreply.github.com>
2024-06-10 12:31:00 +0200

    Time: 42 ms (37.58%) | Memory: 16.3 MB (99.75%) - LeetSync

--- 1137-height-checker/height-checker.py
+++ 1137-height-checker/height-checker.py
@@ -1,15 +1,4 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-        count = [heights.count(i+1) for i in range(100)]
-        i = 0
-        j = 0
-        result = 0
-        while i < len(heights) and j < 100:
-            while count[j] > 0:
-                if heights[i] != j+1:
-                    result += 1
-                count[j] -= 1
-                i += 1
-            j += 1
-        return result
-
+        expected = sorted(heights)
+        return sum(e1 != e2 for e1, e2 in zip(heights, expected))
