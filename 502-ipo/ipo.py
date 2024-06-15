b72758f71760e1c6991b9a52149e127cfea7c8d1
Michał Kobiera <55958194+MicelloK@users.noreply.github.com>
2024-06-15 09:36:01 +0200

    Time: 1108 ms (7.83%) | Memory: 41.3 MB (55.37%) - LeetSync

---
+++ 502-ipo/ipo.py
@@ -0,0 +1,21 @@
+from queue import PriorityQueue
+
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        sorted_projects = sorted(zip(capital, profits))
+        next_proj = 0
+        projects = PriorityQueue()
+        while next_proj < len(sorted_projects) and sorted_projects[next_proj][0] <= w:
+            projects.put(-sorted_projects[next_proj][1])
+            next_proj += 1
+        
+        while not projects.empty():
+            prof = -projects.get()
+            w += prof
+            k -= 1
+            if k == 0:
+                return w
+            while next_proj < len(sorted_projects) and sorted_projects[next_proj][0] <= w:
+                projects.put(-sorted_projects[next_proj][1])
+                next_proj += 1
+        return w
