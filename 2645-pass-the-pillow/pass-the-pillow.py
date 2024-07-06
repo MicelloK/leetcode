a66732c2ef4234048d6db9180e4d0a5fff6bf59e
Michał Kobiera <55958194+MicelloK@users.noreply.github.com>
2024-07-06 15:31:32 +0200

    Time: 36 ms (44.81%) | Memory: 16.6 MB (7.09%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.py
+++ 2645-pass-the-pillow/pass-the-pillow.py
@@ -2,6 +2,4 @@ class Solution:
     def passThePillow(self, n: int, time: int) -> int:
         if (time // (n - 1)) % 2 == 0:
             return 1 + time % (n - 1)
-        else:
-            return n - time % (n - 1)
-        
+        return n - time % (n - 1)
