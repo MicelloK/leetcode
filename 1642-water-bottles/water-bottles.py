02f9ce43ab3e7576e9ad8bc5eab585a0df5d349f
Michał Kobiera <55958194+MicelloK@users.noreply.github.com>
2024-07-07 11:48:37 +0200

    Time: 40 ms (17.64%) | Memory: 16.6 MB (5.42%) - LeetSync

--- 1642-water-bottles/water-bottles.py
+++ 1642-water-bottles/water-bottles.py
@@ -1,5 +1,11 @@
 class Solution:
     def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
-        if numBottles % (numExchange - 1) == 0:
-            return numBottles + numBottles // (numExchange - 1) - 1
-        return numBottles + numBottles // (numExchange - 1)
+        result = numBottles
+        while numBottles >= numExchange:
+            result += numBottles // numExchange
+            numBottles -= (numBottles // numExchange) * (numExchange - 1)
+        return result
+
+        # if numBottles % (numExchange - 1) == 0:
+        #     return numBottles + numBottles // (numExchange - 1) - 1
+        # return numBottles + numBottles // (numExchange - 1)
